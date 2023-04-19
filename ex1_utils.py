444fe5cf4664691de389c6c6694790e2300b77d8
giltzioni <gilqwer1@gmail.com>
2023-04-19 16:11:16 +0300

    fixed

--- ex1_utils.py
+++ ex1_utils.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 
 LOAD_GRAY_SCALE = 1
 LOAD_RGB = 2
+ERROR_MSG = "Error: the given image has wrong dimensions"
 
 
 def myID() -> int:
@@ -23,7 +24,7 @@ def myID() -> int:
     Return my ID (not the friend's ID I copied from)
     :return: int
     """
-    return 315203068
+    return 315203067
 
 
 def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
@@ -33,14 +34,19 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
-    image = cv2.imread(filename)
-    image_norm = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    if representation == 1:
-        ans_img = cv2.cvtColor(image_norm, cv2.COLOR_BGR2GRAY)
-
+    # Loading an image
+    img = cv2.imread(filename)
+    if img is not None:
+        if representation == LOAD_GRAY_SCALE:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        elif representation == LOAD_RGB:
+            # We weren't asked to convert a grayscale image to RGB so this will suffice
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        else:  # Any other value was entered as the second parameter
+            raise ValueError(ERROR_MSG)
     else:
-        ans_img = cv2.cvtColor(image_norm, cv2.COLOR_BGR2RGB)
-    return ans_img
+        raise Exception("Could not read the image! Please try again.")
+    return img / 255.0
 
 def imDisplay(filename: str, representation: int):
     """
@@ -52,7 +58,7 @@ def imDisplay(filename: str, representation: int):
     image = imReadAndConvert(filename, representation)
 
     # Determine the colormap to use for grayscale images
-    cmap = 'gray' if representation == 1 else None
+    cmap = 'gray' if representation == LOAD_GRAY_SCALE else None
 
     # Display the image using matplotlib's imshow function
     plt.imshow(image, cmap=cmap)
@@ -66,20 +72,12 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     :return: A YIQ in image color space
     """
     # Define the transformation matrix
-    A = np.array([[0.299, 0.587, 0.114],
-                  [0.596, -0.275, -0.321],
-                  [0.212, -0.523, 0.311]])
+    yiq_from_rgb = np.array([[0.299, 0.587, 0.114],
+                             [0.596, -0.275, -0.321],
+                             [0.212, -0.523, 0.3111]])
+    OrigShape = imgRGB.shape
+    return np.dot(imgRGB.reshape(-1, 3), yiq_from_rgb.transpose()).reshape(OrigShape)
 
-    # Flatten the image to a 2D array for matrix multiplication
-    im_flat = imgRGB.reshape(-1, 3)
-
-    # Convert the image to YIQ color space using matrix multiplication
-    imYIQ_flat = np.dot(im_flat, A.T)
-
-    # Reshape the image back to its original shape
-    imYIQ = imYIQ_flat.reshape(imgRGB.shape)
-
-    return imYIQ
 
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
@@ -88,21 +86,12 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     :param imgYIQ: An Image in YIQ
     :return: A RGB in image color space
     """
-    # Define the inverse transformation matrix
-    A_inv = np.array([[1.0, 0.956, 0.621],
-                      [1.0, -0.272, -0.647],
-                      [1.0, -1.107, 1.704]])
-
-    # Flatten the image to a 2D array for matrix multiplication
-    im_flat = imgYIQ.reshape(-1, 3)
-
-    # Convert the image to RGB color space using matrix multiplication
-    imRGB_flat = np.dot(im_flat, A_inv.T)
-
-    # Reshape the image back to its original shape
-    imRGB = imRGB_flat.reshape(imgYIQ.shape)
-
-    return imRGB
+    # taking sizes of input to make a new image
+    yiq_from_rgb = np.array([[0.299, 0.587, 0.114],
+                             [0.596, -0.275, -0.321],
+                             [0.212, -0.523, 0.3111]])
+    OrigShape = imgYIQ.shape
+    return np.dot(imgYIQ.reshape(-1, 3), np.linalg.inv(yiq_from_rgb).transpose()).reshape(OrigShape)
 
 
 def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
@@ -167,27 +156,26 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         # If an RGB image is given - convert it to YIQ
     rgb_to_yiq = transformRGB2YIQ(imOrig)
     # z : the borders which divide the histograms into segments
-    qImage_i, error = quantize_chanel(rgb_to_yiq[:, :, 0].copy(), nQuant, nIter)  # take only Y chanel
-    qImage = []
+    qImage_i, MSE_error_list = quantize_chanel(rgb_to_yiq[:, :, 0].copy(), nQuant, nIter)  # take only Y chanel
+    q_img_lst = []
     for img in qImage_i:
         # convert the original img back from YIQ to RGB
         qImage_tmp = transformYIQ2RGB(np.dstack((img, rgb_to_yiq[:, :, 1], rgb_to_yiq[:, :, 2])))  # Rebuilds rgb arrays
-        qImage.append(qImage_tmp)
+        q_img_lst.append(qImage_tmp)
 
-    return qImage, error
+    return q_img_lst, MSE_error_list
 
 
 def quantize_chanel(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
     # return list:
-    qImage = []
-    Error = []
+    q_img_lst = []
+    MSE_error_list = []
 
     # from range [0,1] to [0,255]
     img = imOrig * 255
     # create Histogram
     img_hist, edges = np.histogram(img.flatten(), bins=256)
     cumsum = img_hist.cumsum()
-    # z = calcBoundaries(nQuant)
     z = calcBoundaries(nQuant)
 
     for iter in range(nIter):  # ask for make nIter times
@@ -205,64 +193,13 @@ def quantize_chanel(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.nda
         for border in range(len(q)):
             new_img[img > z[border]] = q[border]
 
-        qImage.append(new_img / 255.0)  # back to range [0,1]
+        q_img_lst.append(new_img / 255.0)  # back to range [0,1]
 
         # Mean Squared Error
         MSE = np.sqrt((img - new_img) ** 2).mean()
-        Error.append(MSE)
+        MSE_error_list.append(MSE)
 
         for i in range(1, len(q)):
             z[i] = (q[i - 1] + q[i]) / 2  # Change the boundaries according to q
 
-    return qImage, Error
-    # # Check if the image is grayscale or RGB
-    # is_gray = (np.ndim(imOrig) == 2)
-    #
-    # # Convert RGB image to YIQ
-    # if not is_gray:
-    #     imOrig = np.dot(imOrig, [0.299, 0.587, 0.114])
-    #
-    # # Initialize the segment division
-    # hist, bins = np.histogram(imOrig.flatten(), 256, [0, 256])
-    # seg_size = imOrig.size // nQuant
-    # cumsum = np.cumsum(hist)
-    # z = np.zeros(nQuant + 1)
-    # for i in range(1, nQuant):
-    #     z[i] = np.interp(i * seg_size, cumsum, bins[:-1])
-    # z[-1] = 255
-    #
-    # # Perform nIter iterations
-    # qImage_list = []
-    # error_list = []
-    # for i in range(nIter):
-    #     # Find the values that each segment will map to
-    #     q = np.zeros(nQuant)
-    #     for j in range(nQuant):
-    #         q[j] = np.average(imOrig[(z[j] <= imOrig) & (imOrig < z[j + 1])])
-    #         # Quantize the image based on the current segment division and values
-    #     qImage = np.zeros_like(imOrig)
-    #     for j in range(nQuant):
-    #        qImage[(z[j] <= imOrig) & (imOrig < z[j + 1])] = q[j]
-    #
-    #     # Calculate the total intensity error
-    #     error = np.sum((imOrig - qImage) ** 2)
-    #
-    #     # Store the results
-    #     qImage_list.append(qImage)
-    #     error_list.append(error)
-    #
-    #     # Update the segment division for the next iteration
-    #     hist, bins = np.histogram(qImage.flatten(), 256, [0, 256])
-    #     cumsum = np.cumsum(hist)
-    #     for j in range(1, nQuant):
-    #          z[j] = np.interp(j * seg_size, cumsum, bins[:-1])
-    #
-    # # Convert quantized image back to RGB if necessary
-    # if not is_gray:
-    #     yiq = np.zeros_like(imOrig)
-    #     yiq[:, :, 0] = qImage
-    #     yiq[:, :, 1:] = imOrig[:, :, 1:]
-    #     qImage_list = [np.dot(img, [[1, 0.956, 0.621], [1, -0.272, -0.647], [1, -1.106, 1.703]]) for img in qImage_list]
-    #
-    # return qImage_list, error_list
-
+    return q_img_lst, MSE_error_list
--- gamma.py
+++ gamma.py
@@ -8,15 +8,14 @@
          ########: ##:::. ##::'######:
         ........::..:::::..:::......::
 """
+from __future__ import print_function
+from __future__ import division
 from ex1_utils import LOAD_GRAY_SCALE
+import numpy as np
 import cv2
 
-def gamma_correction(img , gamma):
-    if(gamma!=0):
-        inv_gamma = 1.0 / gamma
-        table = (255.0 * (img / 255.0) ** inv_gamma).astype('uint8')
-        return table
-    return 0
+alpha_slider_max = 255
+title_window = 'Gamma Correction'
 
 def gammaDisplay(img_path: str, rep: int):
     """
@@ -25,32 +24,32 @@ def gammaDisplay(img_path: str, rep: int):
     :param rep: grayscale(1) or RGB(2)
     :return: None
     """
-    # Load the image
-    img = cv2.imread(img_path)
 
-    # Define the trackbar callback function
-    def trackbar_callback(x):
-        # Convert the trackbar value to gamma
-        gamma = x / 100.0
-        # Apply gamma correction to the image
-        corrected_img = gamma_correction(img, gamma)
-        # Display the corrected image
-        cv2.imshow('Gamma Correction', corrected_img)
+    #  adjusted gamma values
+    global img
+    if rep == 1:  # rep = Gray_Scale representation
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    else:  # =2, LOAD_RGB
+        img = cv2.imread(img_path)
 
-    # Create a window to display the image
-    cv2.namedWindow('Gamma Correction')
+    cv2.namedWindow(title_window)
+    trackbar_name = 'Gamma %d' % alpha_slider_max
+    cv2.createTrackbar(trackbar_name, title_window, 100, alpha_slider_max, trackBar)
+    trackBar(0)
+    print("Click on the screen to close the 'Gamma correction' window")
+    # Keeping the window open until pressing any key
+    cv2.waitKey()
 
-    # Create the trackbar
-    max_value = 200
-    cv2.createTrackbar('Gamma', 'Gamma Correction', 100, max_value, trackbar_callback)
-    # Display the initial image
-    cv2.imshow('Gamma Correction', img)
 
-    # Wait for a key event
-    cv2.waitKey(0)
+def trackBar(n: int):
+    alpha= float(n) / 100
+    invGamma = 1000 if alpha == 0 else 1.0 / alpha
+    max_ = 255
+    table = np.array([((i / float(max_)) ** invGamma) * max_
+                           for i in np.arange(0, max_ + 1)]).astype("uint8")
+    img_ = cv2.LUT(img, table)
+    cv2.imshow(title_window, img_)
 
-    # Destroy all windows
-    cv2.destroyAllWindows()
 
 def main():
     gammaDisplay('bac_con.png', LOAD_GRAY_SCALE)
